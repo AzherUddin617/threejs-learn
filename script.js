67eef9c796c647564198f2a44b311e3fe65f534e
Azher Uddin Ahmed <azher305uddin@gmail.com>
2023-03-24 16:52:15 +0600

    add: camera setup

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
 
 scene.add(mesh);
+
+// Camera setup
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+
+scene.add(camera);
